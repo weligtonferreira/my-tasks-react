3f95f3d4ff93aed173ca9bb79b70868a7359de39
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-03 19:47:35 -0300

    feat: add custom fonts to tailwindcss configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Inter',
+        quicksand: 'Quicksand',
+      },
+    },
   },
   plugins: [],
 };
