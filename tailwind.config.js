e2d818908c1b926fe6e5e000d252bb5bb02053dc
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-03 21:35:54 -0300

    feat: add custom colors to tailwindcss configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'primary-gray': '#fdfdfd',
+        'primary-green': '#228C26',
+        'input-color': '#EAEAEA',
+      },
       fontFamily: {
         sans: 'Inter',
         quicksand: 'Quicksand',
