efdb6f282d5c770ed4f5118dc0ac2d3e14805c58
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-08 09:08:17 -0300

    feat(RegisterPage): add onSubmit in the form element

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -36,7 +36,7 @@ export function RegisterPage() {
             </h2>
 
             <form
-              action=''
+              onSubmit={(event) => handleCreateAnAccount(event)}
               className='flex flex-col items-center justify-center gap-3'
             >
               <div className='flex items-center justify-center bg-input-color rounded-lg px-4 py-2 gap-1'>
