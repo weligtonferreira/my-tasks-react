013b7cdf681eff1bca425d513bdaa3bdb0bc5566
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-25 10:52:13 -0300

    feat(HomePage): add no tasks screen

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/header';
+import { AddButton } from '../../components/add-button';
 
 export function HomePage() {
   return (
@@ -6,7 +7,26 @@ export function HomePage() {
       <Header />
 
       <main className='flex flex-col items-center justify-start h-full w-full p-5'>
-        <h1>Home Page</h1>
+        <div className='flex flex-col items-center justify-center h-full w-full gap-12 p-5'>
+          <img
+            src='/no-tasks.svg'
+            alt='Nenhuma tarefa'
+            className='w-[230px] 2xl:w-[400px]'
+          />
+
+          <div className='flex flex-col items-center justify-center gap-5'>
+            <div className='flex flex-col'>
+              <span className='font-bold text-center text-sm sm:text-base'>
+                Ops... parece que ainda não temos nada ainda
+              </span>
+              <p className='text-center text-xs sm:text-sm md:text-base'>
+                Nenhuma tarefa foi encontrada, crie a sua primeira tarefa!
+              </p>
+            </div>
+
+            <AddButton>Nova tarefa</AddButton>
+          </div>
+        </div>
       </main>
     </div>
   );
