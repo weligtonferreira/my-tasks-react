b0d17c7591f8ba5482ecbe29f6c0658a5541a40e
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-25 11:43:06 -0300

    refactor(AddButton): change name of function in AddButtonProps interface

--- src/components/add-button/index.tsx
+++ src/components/add-button/index.tsx
@@ -3,11 +3,15 @@ import { GoPlus } from 'react-icons/go';
 
 interface AddButtonProps {
   children: ReactNode;
+  openCreateTaskModal: () => void;
 }
 
-export function AddButton({ children }: AddButtonProps) {
+export function AddButton({ children, openCreateTaskModal }: AddButtonProps) {
   return (
-    <div className='flex items-center justify-center max-w-max rounded-md bg-[#1AC014] text-center text-white px-3 py-2 hover:brightness-105 transition-colors hover:cursor-pointer select-none shadow'>
+    <div
+      onClick={openCreateTaskModal}
+      className='flex items-center justify-center max-w-max rounded-md bg-[#1AC014] text-center text-white px-3 py-2 hover:brightness-105 transition-colors hover:cursor-pointer select-none shadow'
+    >
       <GoPlus size={20} />
       <button className='font-semibold font-quicksand pr-1 outline-none border-none'>
         {children}
