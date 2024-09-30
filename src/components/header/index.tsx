97486dcc8dce28750a4736125fdc3d01f57da2ac
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-30 18:08:50 -0300

    feat(Header): add menu to logout user

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { FaAngleDown, FaUser } from 'react-icons/fa';
 
 import { useAuth } from '../../hooks/useAuth';
+import { IoLogOutOutline } from 'react-icons/io5';
 
 export function Header() {
+  const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
   const { user, logout } = useAuth();
 
   return (
@@ -11,9 +14,18 @@ export function Header() {
         My<span className='text-primary-green'>Tasks</span>
       </h1>
 
-      <div className='flex flex-col items-center justify-center rounded-full p-2 bg-gray-100 hover:bg-gray-200 transition-colors hover:cursor-pointer'>
+      <div
+        onMouseOver={() => setIsUserMenuVisible(true)}
+        onMouseOut={() => setIsUserMenuVisible(false)}
+        className='relative flex flex-col items-center justify-center rounded-full p-2 bg-gray-100 hover:bg-light transition-colors hover:cursor-pointer hover:rounded-xl hover:rounded-b-none hover:drop-shadow'
+      >
         <div className='flex items-center justify-center gap-1'>
-          <FaAngleDown size={15} />
+          <FaAngleDown
+            size={15}
+            className={`${
+              isUserMenuVisible ? 'rotate-180' : ''
+            } text-[#363636] transition-all duration-200`}
+          />
 
           <p className='font-sans font-light text-gray-800 text-sm sm:text-base'>
             {user?.name}
@@ -23,6 +35,30 @@ export function Header() {
             <FaUser size={16} />
           </div>
         </div>
+
+        {isUserMenuVisible && (
+          <div
+            className={`${
+              isUserMenuVisible ? 'h-max opacity-100' : 'h-0 opacity-60'
+            } absolute top-[2.5rem] w-full bg-light p-1 rounded-b-lg overflow-hidden transition-all ease-in duration-200`}
+          >
+            <ul className='flex flex-col items-center justify-center w-[inherit] select-none p-1 gap-1'>
+              <hr className='p-1 w-full' />
+
+              <li
+                onClick={logout}
+                className='text-sm text-center hover:font-semibold text-gray-600 hover:text-white bg-gray-100 hover:bg-black px-4 py-2 rounded-md w-full transition-colors'
+              >
+                <p className='flex items-center justify-center gap-1'>
+                  Sair{' '}
+                  <span>
+                    <IoLogOutOutline size={15} />
+                  </span>
+                </p>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </header>
   );
