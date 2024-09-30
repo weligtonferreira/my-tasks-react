ad5c8dab1e19199831b0dd1c80397a628b13dc8d
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-30 16:17:54 -0300

    feat: add handleNotifyValidationErrors function

---
+++ src/utils/handle-notify-validation-erros.ts
@@ -0,0 +1,10 @@
+import { FieldErrors } from 'react-hook-form';
+import { notifyErrorPopUp } from './notify-popups';
+
+export function handleNotifyValidationErrors(formErrors: FieldErrors) {
+  if (Object.keys(formErrors).length > 0) {
+    Object.values(formErrors).forEach((value) =>
+      notifyErrorPopUp(value?.message as string)
+    );
+  }
+}
