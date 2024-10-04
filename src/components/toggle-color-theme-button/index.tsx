9586b215c125e42add8ec4c78c992036fc43dbcd
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-04 15:20:39 -0300

    feat: create ToggleColorThemeButton component

---
+++ src/components/toggle-color-theme-button/index.tsx
@@ -0,0 +1,30 @@
+import { MdOutlineWbSunny } from 'react-icons/md';
+import { useTheme } from '../../hooks/useTheme';
+import { FaMoon } from 'react-icons/fa';
+
+export function ToggleColorThemeButton() {
+  const { theme, toggleThemeColor } = useTheme();
+
+  return (
+    <div
+      onClick={toggleThemeColor}
+      className={`absolute top-8 right-8 ${
+        theme === 'light' ? 'bg-[#ECECEC]' : 'bg-[#5B5B5B]'
+      } flex items-center justify-center gap-3 rounded-full p-2 transition-colors duration-300 cursor-pointer`}
+    >
+      <MdOutlineWbSunny
+        size={25}
+        className={`${
+          theme === 'light' ? 'text-yellow-500' : 'text-slate-400'
+        } cursor-pointer transition-colors duration-300`}
+      />
+
+      <FaMoon
+        size={21}
+        className={`${
+          theme === 'light' ? 'text-gray-400' : 'text-[#9CC5FF]'
+        } cursor-pointer transition-all duration-300`}
+      />
+    </div>
+  );
+}
