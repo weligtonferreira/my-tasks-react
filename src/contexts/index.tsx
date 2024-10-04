ec676d7c466ad9319a12ae073595018a52e48b51
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-04 15:19:43 -0300

    feat(AppContext): add ThemeContext

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,9 +1,14 @@
 import { AuthProvider } from './auth';
+import { ThemeProvider } from './theme';
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 export const AppProvider = ({ children }: AppProviderProps) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </AuthProvider>
+  );
 };
