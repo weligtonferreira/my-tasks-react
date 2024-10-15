11b47122d22f1b3c512f6cbe8426753737a439b1
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-15 12:56:31 -0300

    feat(DetailsTaskModal): adapt to dark theme

--- src/components/details-task-modal/index.tsx
+++ src/components/details-task-modal/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
 
 import { ITask } from '../../dto/ITask';
+import { useTheme } from '../../hooks/useTheme';
 
 interface DetailsTaskModalProps {
   task: ITask;
@@ -14,6 +15,7 @@ export function DetailsTaskModal({
 }: DetailsTaskModalProps) {
   const [bgColor, setBgColor] = useState('bg-black/0');
   const [scaleClass, setScaleClass] = useState('scale-0');
+  const { theme } = useTheme();
 
   function handleCloseModal(event: React.KeyboardEvent<HTMLDivElement>) {
     if (event.key === 'Escape') {
@@ -23,14 +25,19 @@ export function DetailsTaskModal({
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setBgColor('bg-black/20');
+      if (theme === 'light') {
+        setBgColor('bg-black/20');
+      } else {
+        setBgColor('bg-black/40');
+      }
+
       setScaleClass('scale-100');
     }, 0);
 
     return () => {
       clearInterval(timeout);
     };
-  }, []);
+  }, [theme]);
 
   return (
     <div
@@ -41,20 +48,40 @@ export function DetailsTaskModal({
       <div
         className={`${scaleClass} transition-transform duration-300 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] md:w-[500px] modal-shadow rounded-xl rounded-b-xl overflow-hidden`}
       >
-        <div className='relative flex items-center justify-center w-full py-3 bg-[#FAFAFA] border-b rounded-t-xl overflow-hidden'>
-          <p className='font-quicksand font-semibold text-center text-[#505050]'>
+        <div
+          className={`relative flex items-center justify-center w-full py-3 rounded-t-xl overflow-hidden ${
+            theme === 'light'
+              ? 'bg-[#FAFAFA] border-b'
+              : 'bg-[#474747] border-b border-b-[#3A3A3A]'
+          }`}
+        >
+          <p
+            className={`font-quicksand font-semibold text-center ${
+              theme === 'light' ? 'text-[#505050]' : 'text-white'
+            }`}
+          >
             {task.title}
           </p>
 
           <IoMdClose
             size={20}
             onClick={closeDetailsTaskModal}
-            className='absolute right-4 cursor-pointer text-[#757575] hover:text-black transition-colors'
+            className={`absolute right-4 cursor-pointer text-[#757575] ${
+              theme === 'light' ? 'hover:text-black' : 'hover:text-white'
+            } transition-colors`}
           />
         </div>
 
-        <div className='flex items-center justify-center bg-white h-full p-10'>
-          <p className='text-[#707070] text-center'>
+        <div
+          className={`flex items-center justify-center h-full p-10 ${
+            theme === 'light' ? 'bg-white' : 'bg-[#535353]'
+          }`}
+        >
+          <p
+            className={`text-center ${
+              theme === 'light' ? 'text-[#707070]' : 'text-[#EAEAEA]'
+            }`}
+          >
             {task.description !== '' ? task.description : 'Sem descrição'}
           </p>
         </div>
