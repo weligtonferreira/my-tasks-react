8ed4dad83cac2cfcfa14c52445601a402b72af98
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-15 11:27:01 -0300

    feat(TaskMenuOptions): adapt to dark theme

--- src/components/task-menu-options/index.tsx
+++ src/components/task-menu-options/index.tsx
@@ -1,4 +1,5 @@
 import { ITask } from '../../dto/ITask';
+import { useTheme } from '../../hooks/useTheme';
 
 interface TaskMenuOptionsProps {
   task: ITask;
@@ -19,39 +20,69 @@ export function TaskMenuOptions({
   openUpdateTaskModal,
   openDeleteTaskModal,
 }: TaskMenuOptionsProps) {
+  const { theme } = useTheme();
+
   return (
     <div
       className={`${
         isTaskMenuVisible ? 'h-20 border opacity-100' : 'h-0 opacity-60'
-      } z-10 absolute right-0 top-14 bg-[#f3f3f3] rounded-md overflow-hidden transition-all ease-in duration-300`}
+      } z-10 absolute right-0 top-14 ${
+        theme === 'light' ? 'bg-[#f3f3f3]' : 'bg-[#616161] border-[#505050]'
+      } rounded-md overflow-hidden transition-all duration-300`}
     >
-      <ul className='flex flex-col text-center select-none text-[#6F6F6F] cursor-pointer'>
+      <ul className='flex flex-col text-center select-none cursor-pointer'>
         <li
           onClick={() => {
             openDetailsTaskModal(task);
             setIsTaskMenuVisible(false);
           }}
-          className='py-1 px-3 hover:bg-[#e7e7e7] transition-colors'
+          className={`py-1 px-3 ${
+            theme === 'light' ? 'hover:bg-[#e7e7e7]' : 'hover:bg-[#6B6B6B]'
+          } transition-bg-transform duration-bg-transform`}
         >
-          <p className='text-xs text-[#636363] pt-1'>Detalhes</p>
+          <p
+            className={`text-xs ${
+              theme === 'light' ? 'text-[#636363]' : 'text-slate-200'
+            } pt-1 transition-colors duration-300`}
+          >
+            Detalhes
+          </p>
         </li>
+
         <li
           onClick={() => {
             openUpdateTaskModal(task, index);
             setIsTaskMenuVisible(false);
           }}
-          className='py-1 px-3 hover:bg-[#e7e7e7] transition-colors'
+          className={`py-1 px-3 ${
+            theme === 'light' ? 'hover:bg-[#e7e7e7]' : 'hover:bg-[#6B6B6B]'
+          } transition-bg-transform duration-bg-transform`}
         >
-          <p className='text-xs text-[#636363]'>Editar</p>
+          <p
+            className={`text-xs ${
+              theme === 'light' ? 'text-[#636363]' : 'text-slate-200'
+            } transition-colors duration-300`}
+          >
+            Editar
+          </p>
         </li>
+
         <li
           onClick={() => {
             openDeleteTaskModal(task, index);
             setIsTaskMenuVisible(false);
           }}
-          className='py-1 px-3 hover:bg-[#e7e7e7] transition-colors'
+          className={`py-1 px-3 ${
+            theme === 'light' ? 'hover:bg-[#e7e7e7]' : 'hover:bg-[#6B6B6B]'
+          } transition-bg-transform duration-bg-transform`}
         >
-          <p className='text-xs text-[#636363] pb-1'>Excluir</p>
+          <p
+            className={`text-xs ${
+              theme === 'light' ? 'text-[#636363]' : 'text-slate-200'
+            } pb-1 transition-colors duration-300`}
+          >
+            Deletar
+          </p>
         </li>
       </ul>
     </div>
