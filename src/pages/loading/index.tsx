af7a8f47f87eb077e70209d65e4a9f8be9ef4e78
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-15 11:12:29 -0300

    feat: adapt loading page to dark theme

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -1,6 +1,14 @@
+import { useTheme } from '../../hooks/useTheme';
+
 export function LoadingPage() {
+  const { theme } = useTheme();
+
   return (
-    <div className='flex flex-col items-center justify-center bg-light w-screnn h-screen p-4 gap-5'>
+    <div
+      className={`flex flex-col items-center justify-center w-screnn h-screen p-4 gap-5 ${
+        theme === 'light' ? 'bg-white' : 'bg-dark'
+      }`}
+    >
       <img src='/loading.webp' alt='Loading animation' />
 
       <p className='font-bold font-quicksand text-gray-700'>Loading...</p>
