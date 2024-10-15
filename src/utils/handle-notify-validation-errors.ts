1e2182027ed22e78b3820bbe7c0af07b43551420
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-15 18:43:17 -0300

    feat(handleNotifyValidationErrors): add theme to notifyErrorPopUp function

--- src/utils/handle-notify-validation-errors.ts
+++ src/utils/handle-notify-validation-errors.ts
@@ -1,10 +1,13 @@
 import { FieldErrors } from 'react-hook-form';
 import { notifyErrorPopUp } from './notify-popups';
 
-export function handleNotifyValidationErrors(formErrors: FieldErrors) {
+export function handleNotifyValidationErrors(
+  formErrors: FieldErrors,
+  theme: string
+) {
   if (Object.keys(formErrors).length > 0) {
     Object.values(formErrors).forEach((value) =>
-      notifyErrorPopUp(value?.message as string)
+      notifyErrorPopUp(value?.message as string, theme)
     );
   }
 }
