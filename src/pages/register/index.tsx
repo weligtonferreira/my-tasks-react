8f2d70377c4e96682cd17c5c806ceb413e07765b
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-17 19:25:57 -0300

    refactor(RegisterPage): remove setTimeout from useEffect

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -55,14 +55,8 @@ export function RegisterPage() {
   }
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setTranslateToRight('opacity-100 -translate-x-0');
-      setTranslateToDown('opacity-100 -translate-y-0');
-    }, 100);
-
-    return () => {
-      clearTimeout(timeout);
-    };
+    setTranslateToRight('opacity-100 -translate-x-0');
+    setTranslateToDown('opacity-100 -translate-y-0');
   }, []);
 
   return (
