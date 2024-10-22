66ebdb4650d0c276c6e2b36af02577c1ef7f46ab
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-22 10:16:51 -0300

    feat(ToggleColorThemeButton): add a growth effect when the component is mounted

--- src/components/toggle-color-theme-button/index.tsx
+++ src/components/toggle-color-theme-button/index.tsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from 'react';
 import { MdOutlineWbSunny } from 'react-icons/md';
-import { useTheme } from '../../hooks/useTheme';
 import { FaMoon } from 'react-icons/fa';
+
+import { useTheme } from '../../hooks/useTheme';
 
 export function ToggleColorThemeButton() {
   const { theme, toggleThemeColor } = useTheme();
+  const [growUpScale, setGrowUpScale] = useState('opacity-0 scale-50');
+  const [durationBgTransformOpacity, setDurationBgTransformOpacity] = useState(
+    'duration-bg-transform-opacity'
+  );
+
+  useEffect(() => {
+    setGrowUpScale('opacity-100 scale-100');
+
+    const timeout = setTimeout(() => {
+      setDurationBgTransformOpacity('duration-bg-transform-opacity-second');
+    }, 1500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
 
   return (
     <div
       onClick={toggleThemeColor}
-      className={`fixed z-20 bottom-12 right-14 shadow ${
+      className={`${growUpScale} fixed z-20 bottom-12 right-14 shadow ${
         theme === 'light' ? 'bg-[#ECECEC]' : 'bg-[#5B5B5B]'
-      } flex items-center justify-center gap-3 rounded-full p-2 hover:scale-110 hover:brightness-95 transition-bg-transform duration-bg-transform cursor-pointer`}
+      } flex items-center justify-center gap-3 rounded-full p-2 hover:scale-110 hover:brightness-95 transition-bg-transform-opacity ${durationBgTransformOpacity} cursor-pointer`}
     >
       <MdOutlineWbSunny
         size={25}
