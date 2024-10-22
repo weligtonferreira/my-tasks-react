4ec169a7b5f461cf3359f14a459e989394f2b1e0
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-10-22 10:42:07 -0300

    feat(AddButton): add transition to brightness effect

--- src/components/add-button/index.tsx
+++ src/components/add-button/index.tsx
@@ -10,7 +10,7 @@ export function AddButton({ children, openCreateTaskModal }: AddButtonProps) {
   return (
     <div
       onClick={openCreateTaskModal}
-      className='flex items-center justify-center max-w-max rounded-md bg-[#1AC014] text-center text-white px-3 py-2 hover:brightness-105 transition-colors hover:cursor-pointer select-none shadow'
+      className='flex items-center justify-center max-w-max rounded-md bg-[#1AC014] text-center text-white px-3 py-2 hover:brightness-105 transition-[filter] duration-200 hover:cursor-pointer select-none shadow'
     >
       <GoPlus size={20} />
       <button className='font-semibold font-quicksand pr-1 outline-none border-none'>
