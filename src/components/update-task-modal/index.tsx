087239a4d7202daedc5fe0ddd151ca4d3884e095
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-11-18 14:23:15 -0300

    refactor(UpdateTaskModal): remove setTimeout in useEffect

--- src/components/update-task-modal/index.tsx
+++ src/components/update-task-modal/index.tsx
@@ -97,19 +97,13 @@ export function UpdateTaskModal({
   }, [isUpdateTaskModalOpen]);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (theme === 'light') {
-        setBgColor('bg-black/20');
-      } else {
-        setBgColor('bg-black/40');
-      }
+    if (theme === 'light') {
+      setBgColor('bg-black/20');
+    } else {
+      setBgColor('bg-black/40');
+    }
 
-      setScaleClass('scale-100');
-    }, 0);
-
-    return () => {
-      clearInterval(timeout);
-    };
+    setScaleClass('scale-100');
   }, [theme]);
 
   return (
